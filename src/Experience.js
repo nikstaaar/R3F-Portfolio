c085a7e829ae8489b0926eba5537ea70590a8ee4
Your Name <niktjawalla922@gmail.com>
2023-05-04 15:13:12 +0200

    add laptop

--- src/Experience.js
+++ src/Experience.js
@@ -1,17 +1,35 @@
-import { OrbitControls } from '@react-three/drei'
+import { ContactShadows, Float, useGLTF, PresentationControls, Environment } from '@react-three/drei'
 
 export default function Experience()
 {
+    const computer = useGLTF('https://vazxmixjsiawhamofees.supabase.co/storage/v1/object/public/models/macbook/model.gltf')
+
     return <>
+
+        <Environment preset="city"></Environment>
 
         <color args={[ '#695b5b' ]} attach="background"></color>
 
-        <OrbitControls makeDefault />
-        
-        <mesh>
-            <boxGeometry />
-            <meshNormalMaterial />
-        </mesh>
-
+        <PresentationControls 
+        global
+        rotation={[0.13,0.1,0]}
+        polar={[-0.4, 0.2]}
+        azimuth={[ -1.0, 0.75]}
+        config={{mass:2, tension: 400}}
+        snap={{mass:4, tension: 400}}
+        >
+        <Float rotationIntensity={ 0.4 }>
+        <primitive 
+            object={computer.scene}
+            position-y={-1.2}
+        />
+        </Float  >
+        </PresentationControls>
+        <ContactShadows 
+        position-y={-1.4}
+        opacity={0.4}
+        scale={5}
+        blur={2.4}
+        />
     </>
 }
